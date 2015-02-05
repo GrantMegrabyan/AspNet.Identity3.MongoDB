261400a24a75af367724f0bf30858ec7ecb86cc0
Grant Megrabyan <grant.megrabyan@gmail.com>
2015-02-06 01:39:32 +0300

    Add tests for MongoIdentityUser.RemoveClaim()

--- test/Tests/MongoIdentityUserTests.cs
+++ test/Tests/MongoIdentityUserTests.cs
@@ -87,5 +87,99 @@ namespace Tests
 
             Assert.Empty(user.MyClaims);
         }
+
+        [Fact]
+        public void UserWithOneClaim_RemoveNonExistingClaim_DoesNothing()
+        {
+            var user = new MongoIdentityUser();
+
+            string claimType = "test-claim";
+            string claimValue = "test-value";
+
+            var claim = new Claim(claimType, claimValue);
+            user.AddClaim(claim);
+
+            user.RemoveClaim(new Claim("non-existing-type", "non-existing-value"));
+
+            Assert.Equal(1, user.MyClaims.Count);
+            Assert.Equal(claimType, user.MyClaims[0].Type);
+            Assert.Equal(claimValue, user.MyClaims[0].Value);
+        }
+
+        [Fact]
+        public void UserWithOneClaim_RemoveClaimWithTheSameTypeButDifferentValue_DoesNothing()
+        {
+            var user = new MongoIdentityUser();
+
+            string claimType = "test-claim";
+            string claimValue = "test-value";
+
+            var claim = new Claim(claimType, claimValue);
+            user.AddClaim(claim);
+
+            user.RemoveClaim(new Claim(claimType, "non-existing-value"));
+
+            Assert.Equal(1, user.MyClaims.Count);
+            Assert.Equal(claimType, user.MyClaims[0].Type);
+            Assert.Equal(claimValue, user.MyClaims[0].Value);
+        }
+
+        [Fact]
+        public void UserWithOneClaim_RemoveClaimWithTheSameValueButDifferentType_DoesNothing()
+        {
+            var user = new MongoIdentityUser();
+
+            string claimType = "test-claim";
+            string claimValue = "test-value";
+
+            var claim = new Claim(claimType, claimValue);
+            user.AddClaim(claim);
+
+            user.RemoveClaim(new Claim("non-existing-type", claimValue));
+
+            Assert.Equal(1, user.MyClaims.Count);
+            Assert.Equal(claimType, user.MyClaims[0].Type);
+            Assert.Equal(claimValue, user.MyClaims[0].Value);
+        }
+
+        [Fact]
+        public void UserWithOneClaim_RemoveTheClaim_NoClaimsRemain()
+        {
+            var user = new MongoIdentityUser();
+
+            string claimType = "test-claim";
+            string claimValue = "test-value";
+
+            var claim1 = new Claim(claimType, claimValue);
+            var claim2 = new Claim(claimType, claimValue);
+
+            user.AddClaim(claim1);
+            user.RemoveClaim(claim2);
+
+            Assert.Empty(user.MyClaims);
+        }
+
+        [Fact]
+        public void UserWithTwoClaims_RemoveOneOfTheClaims_OtherClaimRemains()
+        {
+            var user = new MongoIdentityUser();
+
+            string claimType1 = "test-claim-1";
+            string claimValue1 = "test-value-1";
+            var claim1 = new Claim(claimType1, claimValue1);
+
+            string claimType2 = "test-claim-2";
+            string claimValue2 = "test-value-2";
+            var claim2 = new Claim(claimType2, claimValue2);
+
+            user.AddClaim(claim1);
+            user.AddClaim(claim2);
+
+            user.RemoveClaim(new Claim(claimType1, claimValue1));
+
+            Assert.Equal(1, user.MyClaims.Count);
+            Assert.Equal(claimType2, user.MyClaims[0].Type);
+            Assert.Equal(claimValue2, user.MyClaims[0].Value);
+        }
     }
 }
